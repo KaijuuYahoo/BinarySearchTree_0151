58b1f2c93e0814ab69d9cc8ee57c08258ad8c0ee
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-06-26 15:49:24 +0700

    step 6: If the value in the data field of new node is less than that of parent

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -64,6 +64,15 @@ class BinaryTree{
                 //5b: exit
                 return; 
             }
+            //6: If the value in the data field of new node is less than that of parent
+            if (x < parent->info)
+            {
+                //6a: Make the left child of parent point to the new node
+                parent ->leftchild = newNode;
+                
+                //6b: Exit
+                return; 
+            }
 
         }
 };
